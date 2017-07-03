c4b46cf4b57bcbda7f5ea82951e1bfeaca33058a
Administrator <admin@example.com>
2017-07-04 01:18:56 +0800

    edit history

--- src/App.js
+++ src/App.js
@@ -7,42 +7,57 @@ class App extends Component {
     constructor(props) {
         // 如果有寫constructor就一定要搭配super
         super(props);       
-        this.handleLessClick = this.handleLessClick.bind(this);
+        //this.handleLessClick = this.handleLessClick.bind(this);
         this.state = { 
             count: '',
+            plus: '',
             list: []
         };
     }
 
-    //handleAddClick = () =>{} ，這樣子的寫法稱為 arrow Function
+    //handleCountClick = () =>{} ，這樣子的寫法稱為 arrow Function
     //直接監控component 的 this參數 ，所以就不需要多宣告bind
-    handleAddClick = () => {
-        const item = this.state.list;
-        const vul = Number.parseInt(this.state.count) + 1;
-        item.push(vul)
-        if (this.state.count !== '') {
-            this.setState({ 
-                count: vul,
-                list:item
-            });
-        }        
-    }
-    handleLessClick() {
-        const item = this.state.list;
-        const vul = Number.parseInt(this.state.count) - 1;
-        item.push(vul)
-        if (this.state.count !== 0) {
-            this.setState({ 
-                count: vul,
-                list:item
-             });
-        }
-    }
-    handleValue = (e) => {
-        const item = this.state.list;
-        item.push(e.target.value);
+    handleCountClick = (e) => {
+        var item = this.state.list;
+        const vul = Number.parseInt(this.state.count);
+        //item.push(vul,e.target.value);
         this.setState({ 
-            count: this.state.count + e.target.value,
+                count: vul,
+                plus: e.target.value,
+                list:item
+            });    
+    }
+
+    handleValue = (e) => {   
+        var item = this.state.list;
+        var vul = this.state.count!='' ? this.state.count : e.target.value;
+        const plus  = this.state.plus;
+        
+        if (plus !== '') {
+            switch (plus) {
+            case '+':
+                vul = Number.parseInt(vul) + Number.parseInt(e.target.value);
+                break;
+            case '-':
+                vul = Number.parseInt(vul) - Number.parseInt(e.target.value);
+                break;
+            default:
+                alert('error');
+                break;
+          };
+        }
+        else
+        {
+            vul = this.state.count + e.target.value;
+        }
+
+        if (plus !== "") {
+            item.push(e.target.value +","+ plus);
+        }
+        
+        this.setState({ 
+            count: vul, //this.state.count + 
+            plus: '',
             list: item
         });
     }
@@ -50,9 +65,10 @@ class App extends Component {
     handleClear = () => {
         this.setState({ count:  this.state.count = '' });
     }
+
     render() {
         const count = this.state.count;
-
+        const plus  = this.state.plus;
         const warpList = this.state.list.map((data, index) => {
              return <ShowHistoryList  key={index}  history={data}  numkey={index} />
         })
@@ -74,11 +90,11 @@ class App extends Component {
                                 {/*this.handleValue.bind(this),bind會導致效能不好,因為每bind一次就會create instance*/}
                             </td>
                             <td>
-                                <input type="button" onClick={this.handleAddClick} value="+" /><br />
+                                <input type="button" onClick={this.handleCountClick} value="+" /><br />
                                 {/*如果funciot 要傳入固定的參數，onClick function就必須多arrow function handle
                                    onClick={() => this.handleLessClick('ABC')}
                                 */}
-                                <input type="button" onClick={this.handleLessClick} value="--" />
+                                <input type="button" onClick={this.handleCountClick} value="-" />
                             </td>
                             <td>
                                 <ul>
--- src/ShowHistoryList.js
+++ src/ShowHistoryList.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
+class ShowHistoryList extends Component {
 
-const ShowHistoryList = ({history, numkey}) => {
-    return(       
-        <div>   
-            <li>
-                name:{history},key:{numkey} 
+    handelClick = (e) =>{ 
+        
+    }
+    render(){       
+     return(
+            <li onClick ={() =>this.handelClick(this.props.numkey)}>
+                {this.props.history} &nbsp;
             </li>
-        </div>       
-    )
+        );
+    }
 }
+
+
+// const ShowHistory = ({history, numkey}) => {
+//      const filterDrinkList = list.filter((numkey) => {
+//             return data.type === 'drink';
+//         })
+//     return(       
+//         <div>   
+//             <li>
+//                 name:{history},key:{numkey} 
+//             </li>
+//         </div>       
+//     )
+// }
 
 
 module.exports = ShowHistoryList;
